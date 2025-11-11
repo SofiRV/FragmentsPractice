39d47066d802266e09bb0d73ac76e0ac9512c554
Sofi <srodriguezvanegas@cifpfbmoll.eu>
2025-11-11 19:52:55 +0100

    Final version with animations

--- app/src/main/java/com/example/fragmentspractice/MainActivity.java
+++ app/src/main/java/com/example/fragmentspractice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fragmentspractice;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,34 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnFragmentOne = findViewById(R.id.btn_fragment_one);
+        Button btnFragmentTwo = findViewById(R.id.btn_fragment_two);
+
+        btnFragmentOne.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(
+                            R.anim.slide_in_right,
+                            R.anim.slide_out_left,
+                            R.anim.slide_in_left,
+                            R.anim.slide_out_right
+                    )
+                    .replace(R.id.fragment_container, new FirstFragment())
+                    .addToBackStack(null)
+                    .commit();
+        });
+
+        btnFragmentTwo.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(
+                            R.anim.slide_in_right,
+                            R.anim.slide_out_left,
+                            R.anim.slide_in_left,
+                            R.anim.slide_out_right
+                    )
+                    .replace(R.id.fragment_container, new SecondFragment())
+                    .addToBackStack(null)
+                    .commit();
         });
     }
 }
